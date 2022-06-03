08c2b78a3a2555cf0b57b30e3933bf21edbf8613
linhongpeng <709564555@qq.com>
2022-06-03 23:24:22 +0800

    init spring-debug module

---
+++ spring-debug/src/main/java/com/hp/Main.java
@@ -0,0 +1,25 @@
+package com.hp;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author linhongpeng
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		//1. 创建 Spring 的 IOC 容器
+		AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		//2. 从 IOC 容器中获取 bean 的实例
+		HelloWorld helloWorld = ctx.getBean(HelloWorld.class);
+
+		// 根据类型来获取 bean 的实例: 要求在  IOC 容器中只有一个与之类型匹配的 bean, 若有多个则会抛出异常.
+		// 一般情况下, 该方法可用, 因为一般情况下, 在一个 IOC 容器中一个类型对应的 bean 也只有一个.
+		// HelloWorld helloWorld1 = ctx.getBean(HelloWorld.class);
+		//3. 使用 bean
+		helloWorld.hello();
+	}
+
+}
